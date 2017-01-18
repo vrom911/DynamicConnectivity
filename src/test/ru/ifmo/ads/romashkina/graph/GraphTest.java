cde6fda39503122cc9a87263fbf37cbd98fa49ce
vrom911 <vrom911@gmail.com>
2017-01-18 21:52:11 +0300

    Graph Hand Tests for cut() and some utilities added

--- src/test/ru/ifmo/ads/romashkina/graph/GraphTest.java
+++ src/test/ru/ifmo/ads/romashkina/graph/GraphTest.java
@@ -2,12 +2,20 @@ package ru.ifmo.ads.romashkina.graph;
 
 import org.junit.Before;
 import org.junit.Test;
+import ru.ifmo.ads.romashkina.euler.EulerTourTree;
 import ru.ifmo.ads.romashkina.treap.ImplicitTreap;
+import ru.ifmo.ads.romashkina.treap.ImplicitTreapPair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static ru.ifmo.ads.romashkina.euler.EulerTourTree.cut;
 import static ru.ifmo.ads.romashkina.euler.EulerTourTree.link;
 import static ru.ifmo.ads.romashkina.euler.EulerUtility.findEulerPath;
 import static ru.ifmo.ads.romashkina.graph.GraphTestUtility.*;
+import static ru.ifmo.ads.romashkina.graph.GraphUtility.makeGraphFromFile;
 import static ru.ifmo.ads.romashkina.treap.ImplicitTreap.makeValueList;
 
 public class GraphTest {
@@ -18,8 +26,8 @@ public class GraphTest {
 
     @Test
     public void GraphInitTest() {
-        Graph graph1 = new Graph("graphTest.txt");
-        Graph graph2 = new Graph("graphTest1.txt");
+        Graph graph1 = makeGraphFromFile("graphTest.txt");
+        Graph graph2 = makeGraphFromFile("graphTest1.txt");
 
         ImplicitTreap<Vertex> res1 = makeEulerPathTree(graph1, graph1.getVertex(2));
         ImplicitTreap<Vertex> res2 = makeEulerPathTree(graph2, graph2.getVertex(0));
@@ -30,8 +38,8 @@ public class GraphTest {
 
     @Test
     public void eulerTourTest() {
-        Graph graph1 = new Graph("graphTest.txt");
-        assertEquals("a c d c b c a", labelsFromVertices(findEulerPath(graph1.getVertex(0), graph1.getVertexNum())));
+        Graph graph1 = makeGraphFromFile("graphTest.txt");
+        assertEquals("a c d c b c a", labelsFromVertices(makeValueList(findEulerPath(graph1.getVertex(0), graph1.getVertexNum()))));
     }
 
     @Test
@@ -48,22 +56,79 @@ public class GraphTest {
 
     @Test
     public void oneVertexLinkHandTest() {
-        handTestTemplate("a", "b", 0, 0, "a b a");
+        String result = handLinkTestToString("a", "b", 0, 0);
+        assertEquals("a b a", result);
     }
 
     @Test
     public void oneVertexToManyLinkHandTest() {
-        handTestTemplate("a", "b c b", 0, 0, "a b c b a");
-        handTestTemplate("a b a", "c", 0, 0, "a c a b a");
+        String result = handLinkTestToString("a", "b c b", 0, 0);
+        assertEquals("a b c b a", result);
+
+        result = handLinkTestToString("a b a", "c", 0, 0);
+        assertEquals("a b a c a", result);
     }
 
     @Test
     public void paperExampleLinkHandTest() {
-        handTestTemplate("c a c b c d c", "e g f g h g e", 6, 5, "c a c b c d c g e g f g h g c");
+        String result = handLinkTestToString("c a c b c d c", "e g f g h g e", 6, 5);
+        assertEquals("c a c b c d c g e g f g h g c", result);
     }
 
     @Test
-    public void handLinkHandTest() {
-        handTestTemplate("a b d b c e c b a", "f g j k j i j g h g f", 6, 1, "a b d b c e c g j k j i j g h g f g c b a");
+    public void linkHandTest() {
+        String result = handLinkTestToString("a b d b c e c b a", "f g j k j i j g h g f", 6, 1);
+        assertEquals("a b d b c e c g f g j k j i j g h g c b a", result);
+    }
+
+    @Test
+    public void doubleLinkTest() {
+        ImplicitTreap<Vertex> linkTree = handLinkTest("a", "b", 0, 0);
+        assertEquals("a b a", labelsFromTour(linkTree));
+        ImplicitTreap<Vertex> addTree = makeEulerFromString("c");
+        String result = labelsFromTour(link(linkTree.getValue(), addTree.getValue()));
+        assertEquals("a c a b a", result);
+    }
+
+    @Test
+    public void oneEdgeCutTest() {
+        handCutTest(0, "a b a", "a", "b");
+    }
+
+    @Test
+    public void oneEdgeReverseCutTest() {
+        handCutTest(1, "a b a", "a", "b");
+    }
+
+    @Test
+    public void simpleCutTest() {
+        handCutTest(0, "a c b c a", "a", "c b c");
+        handCutTest(3, "a c b c a", "a", "c b c");
+    }
+
+    @Test
+    public void lectureCutTest() {
+        handCutTest(3, "c e c g j k j i j g h g f g c", "c e c g h g f g c", "j k j i j");
+    }
+
+    @Test
+    public void otherCutTest() {
+        handCutTest(4, "c a c b c g h g e g f g c d c", "c a c b c d c", "g h g e g f g");
+    }
+
+    @Test
+    public void linkCutLinkTest() {
+        Vertex a = new Vertex("a", 0);
+        a.setIn(new ImplicitTreap<>(EulerTourTree.random.nextLong(), a));
+        Vertex b = new Vertex("b", 1);
+        b.setIn(new ImplicitTreap<>(EulerTourTree.random.nextLong(), b));
+        ImplicitTreap<Vertex> firstLink = link(a, b);
+        List<Vertex> firstLinkVerticies = new ArrayList<>(makeValueList(firstLink));
+        ImplicitTreapPair<Vertex> cutResult = cut(a, b);
+        assertEquals(a, cutResult.getFirst().getValue());
+        assertEquals(b, cutResult.getSecond().getValue());
+        ImplicitTreap<Vertex> secondLink = link(cutResult.getFirst().getValue(), cutResult.getSecond().getValue());
+        assertEquals(firstLinkVerticies, makeValueList(secondLink));
+        assertTrue(firstLink.getValue().getEdges().keySet().contains(b));
     }
 }
--- src/test/ru/ifmo/ads/romashkina/graph/GraphTestUtility.java
+++ src/test/ru/ifmo/ads/romashkina/graph/GraphTestUtility.java
@@ -1,12 +1,16 @@
 package ru.ifmo.ads.romashkina.graph;
 
+import ru.ifmo.ads.romashkina.euler.EulerTourTree;
 import ru.ifmo.ads.romashkina.treap.ImplicitTreap;
+import ru.ifmo.ads.romashkina.treap.ImplicitTreapPair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.StringJoiner;
 
 import static org.junit.Assert.assertEquals;
+import static ru.ifmo.ads.romashkina.euler.EulerTourTree.cut;
 import static ru.ifmo.ads.romashkina.euler.EulerTourTree.link;
 import static ru.ifmo.ads.romashkina.euler.EulerUtility.findEulerPath;
 import static ru.ifmo.ads.romashkina.treap.ImplicitTreap.*;
@@ -16,25 +20,45 @@ public class GraphTestUtility {
     public static ImplicitTreap<Vertex> makeEulerPathTreeFromList(List<Vertex> vertices) {
         List<ImplicitTreap<Vertex>> vertexTreaps = new ArrayList<>(vertices.size());
         for (Vertex v : vertices) {
-            ImplicitTreap<Vertex> curVertexTreap = new ImplicitTreap<>(v);
+            ImplicitTreap<Vertex> curVertexTreap = new ImplicitTreap<>(EulerTourTree.random.nextLong(), v);
             vertexTreaps.add(curVertexTreap);
             v.setIn(curVertexTreap);
+
+            int  curTreapSize = vertexTreaps.size();
+            if (curTreapSize > 1) {
+                Vertex previousVertex = vertices.get(curTreapSize - 2);
+                Edge edge = new Edge(previousVertex, v, vertexTreaps.get(curTreapSize - 2), curVertexTreap);
+                previousVertex.addEdge(edge);
+            }
         }
         return mergeTreapList(vertexTreaps);
     }
 
     public static ImplicitTreap<Vertex> makeEulerPathTree(Graph graph, Vertex u) {
-        return makeEulerPathTreeFromList(findEulerPath(u, graph.getVertexNum()));
+        return findEulerPath(u, graph.getVertexNum());
     }
 
     public static ImplicitTreap<Vertex> makeEulerPathTreeRandomVertex(Graph graph) {
         return  makeEulerPathTree(graph, graph.getRandomVertex());
     }
 
+    public static ImplicitTreap<Vertex> makeEulerFromString(String l) {
+        return makeEulerPathTreeFromList(createVerticesFromLabels(l.split(" ")));
+    }
+
     public static List<Vertex> createVerticesFromLabels(String[] labels) {
+        HashMap<String, Vertex> vertexMap = new HashMap<>();
         List<Vertex> vertices = new ArrayList<>();
         for (int i = 0; i < labels.length; i++) {
-            vertices.add(new Vertex(labels[i], i));
+            String label = labels[i];
+            Vertex v;
+            if (vertexMap.containsKey(label)) {
+                v = vertexMap.get(label);
+            } else {
+                v = new Vertex(label, i);
+                vertexMap.put(label, v);
+            }
+            vertices.add(v);
         }
         return vertices;
     }
@@ -51,11 +75,23 @@ public class GraphTestUtility {
         return labelsFromVertices(makeValueList(tour));
     }
 
-    public static void handTestTemplate(String l1, String l2, int v1, int v2, String result) {
+    public static ImplicitTreap<Vertex> handLinkTest(String l1, String l2, int v1, int v2) {
         List<Vertex> eulerTour1 = createVerticesFromLabels(l1.split(" "));
         List<Vertex> eulerTour2 = createVerticesFromLabels(l2.split(" "));
         makeEulerPathTreeFromList(eulerTour1);
         makeEulerPathTreeFromList(eulerTour2);
-        assertEquals(result, labelsFromTour(link(eulerTour1.get(v1), eulerTour2.get(v2))));
+        return  link(eulerTour1.get(v1), eulerTour2.get(v2));
+    }
+
+    public static String handLinkTestToString(String l1, String l2, int v1, int v2) {
+        return  labelsFromTour(handLinkTest(l1, l2, v1, v2));
+    }
+
+    public static void handCutTest(int edge, String l, String result1, String result2) {
+        List<Vertex> eulerTour1 = createVerticesFromLabels(l.split(" "));
+        makeEulerPathTreeFromList(eulerTour1);
+        ImplicitTreapPair<Vertex> cutResult = cut(eulerTour1.get(edge), eulerTour1.get(edge + 1));
+        assertEquals(result1, labelsFromTour(cutResult.getFirst()));
+        assertEquals(result2, labelsFromTour(cutResult.getSecond()));
     }
 }
